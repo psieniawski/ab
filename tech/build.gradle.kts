bad92109bc42ddee7717c04b40146c4ad6b7f564
psieniawski <piotr.sieniawski@gmail.com>
2023-02-14 14:34:30 +0100

    Gradle bumped up to 8.0 Kotlin bumped up to 1.8.10

--- tech/build.gradle.kts
+++ tech/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins{
-    kotlin("jvm") version "1.8.0" apply false
-    kotlin("plugin.spring") version "1.8.0" apply false
+    kotlin("jvm") version "1.8.10" apply false
+    kotlin("plugin.spring") version "1.8.10" apply false
 }
 
 
