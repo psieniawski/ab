cef5f051706cae44f03fef4c7ef0f7152b0d0205
psieniawski <piotr.sieniawski@gmail.com>
2023-03-07 16:42:03 +0100

    Prometheus metrics endpoint added

--- tech/boot-app/build.gradle.kts
+++ tech/boot-app/build.gradle.kts
@@ -6,5 +6,5 @@ group = "ab.tech"
 dependencies {
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-//    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
 }
