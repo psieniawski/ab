940ec9ef863be4c863626a687e63384b269b9bf6
psieniawski <piotr.sieniawski@gmail.com>
2023-04-21 23:44:07 +0200

    Back to distributed

--- dummy-ab/boot-app/build.gradle.kts
+++ dummy-ab/boot-app/build.gradle.kts
@@ -13,11 +13,11 @@ dependencies {
     implementation(project(":spring-rest-api"))
 
 // for distributed
-//    implementation("ab.dummy-a:rest-impl")
-//    implementation("ab.dummy-b:rest-impl")
+    implementation("ab.dummy-a:rest-impl")
+    implementation("ab.dummy-b:rest-impl")
 
 // for monolith
-    implementation("ab.dummy-a:impl-spring-boot-starter")
-    implementation("ab.dummy-b:impl-spring-boot-starter")
+//    implementation("ab.dummy-a:impl-spring-boot-starter")
+//    implementation("ab.dummy-b:impl-spring-boot-starter")
 
 }
