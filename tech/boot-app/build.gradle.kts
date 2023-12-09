bd6481f20dd98e887db0149a479d6016b15e5df6
psieniawski <piotr.sieniawski@gmail.com>
2023-12-09 17:12:58 +0100

    Bumped up tool versions

--- tech/boot-app/build.gradle.kts
+++ tech/boot-app/build.gradle.kts
@@ -7,4 +7,5 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-autoconfigure")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
+    runtimeOnly("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 }
